2053578c9b0b5f69a9855cf54962f679cc1a4e55
Smit <aquarocker20@gmail.com>
2022-01-20 16:18:40 +0530

    first commit

---
+++ src/components/bio.js
@@ -0,0 +1,57 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+const Bio = () => {
+  const { author } = useStaticQuery(graphql`
+    query BioQuery {
+      # if there was more than one user, this would need to be filtered
+      author: wpUser {
+        firstName
+        twitter: name
+        description
+        avatar {
+          url
+        }
+      }
+    }
+  `)
+
+  console.log("author", author)
+
+  const avatarUrl = author?.avatar?.url
+
+  return (
+    <div className="bio">
+      {avatarUrl && (
+        <span>
+          <img
+            alt={author?.firstName || ``}
+            className="bio-avatar"
+            src={avatarUrl}
+          />
+        </span>
+      )}
+      {author?.firstName && (
+        <p>
+          Written by <strong>{author.firstName}</strong>
+          {author?.description ? (`, ` + author?.description || null) : `. `}
+          {` `}
+          {author?.twitter && (
+            <a href={`https://twitter.com/${author?.twitter || ``}`}>
+              You should follow him on Twitter
+            </a>
+          )}
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default Bio
